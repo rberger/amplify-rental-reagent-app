2061a6cb11f7ca941967fd73d4d7435ee6984daf
Robert J. Berger <rob@omnyway.com>
2021-12-21 14:14:48 -0800

    Snapshot before doing npm audit fix --force

--- src/main/amplify_rental_reagent_app/app/core.cljs
+++ src/main/amplify_rental_reagent_app/app/core.cljs
@@ -1,13 +1,20 @@
 (ns amplify-rental-reagent-app.app.core
-  (:require [reagent.dom :as rdom]))
+  (:require [reagent.dom :as rdom]
+            ["/aws-exports" :default ^js aws-exports]
+            ["aws-amplify" :default Amplify]
+            ["@aws-amplify/ui-react" :default AmplifyProvider]
+            ["./ui-components" :default RentalCollection]))
 
 (defn app []
-  [:h1 "Create Reagent App"])
+  [:h1 "Create Reagent App"]
+  [:> AmplifyProvider
+   [:> RentalCollection]])
 
 (defn render []
   (rdom/render [app] (.getElementById js/document "root")))
 
 (defn ^:export main []
+  (-> Amplify (.configure aws-exports))
   (render))
 
 (defn ^:dev/after-load reload! []
