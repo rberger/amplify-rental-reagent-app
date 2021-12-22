7078723e5876959c4ac53a29e032c0237b4cf6ff
Robert J. Berger <rob@omnyway.com>
2021-12-21 23:09:13 -0800

    Trying to get jsx converted to js

--- src/main/amplify_rental_reagent_app/app/core.cljs
+++ src/main/amplify_rental_reagent_app/app/core.cljs
@@ -1,20 +1,22 @@
 (ns amplify-rental-reagent-app.app.core
   (:require [reagent.dom :as rdom]
-            ["/aws-exports" :default ^js aws-exports]
-            ["aws-amplify" :default Amplify]
-            ["@aws-amplify/ui-react" :default AmplifyProvider]
-            ["./ui-components" :default RentalCollection]))
+            ;; ["/aws-exports" :default ^js aws-exports]
+            ;; ["aws-amplify" :default Amplify]
+            ;; ["@aws-amplify/ui-react" :default AmplifyProvider]
+            ;; ["../../ui-components" :refer [CardACollection]]
+            ))
 
 (defn app []
   [:h1 "Create Reagent App"]
-  [:> AmplifyProvider
-   [:> RentalCollection]])
+  ;; [:> AmplifyProvider
+  ;;  [:> CardACollection]]
+  )
 
 (defn render []
   (rdom/render [app] (.getElementById js/document "root")))
 
 (defn ^:export main []
-  (-> Amplify (.configure aws-exports))
+  ;; (-> Amplify (.configure aws-exports))
   (render))
 
 (defn ^:dev/after-load reload! []
